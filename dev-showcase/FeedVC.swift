ed6dd5d39327ccbdd97e50cbc502e4c5d8b35f7a
ryan long <ryan.e.long@gmail.com>
2017-02-08 22:12:21 -0500

    Added a tableview with sample data once logged in

---
+++ dev-showcase/FeedVC.swift
@@ -0,0 +1,35 @@
+//
+//  FeedVC.swift
+//  dev-showcase
+//
+//  Created by ryan on 2/8/17.
+//  Copyright © 2017 ryan. All rights reserved.
+//
+
+import UIKit
+
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+    }
+    
+}
---
+++ dev-showcase/PostCell.swift
@@ -0,0 +1,33 @@
+//
+//  PostCell.swift
+//  dev-showcase
+//
+//  Created by ryan on 2/8/17.
+//  Copyright © 2017 ryan. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var showcaseImg: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+
+    override func draw(_ rect: CGRect) {
+        profileImg.layer.cornerRadius = profileImg.frame.size.width / 2
+        profileImg.clipsToBounds = true
+        showcaseImg.clipsToBounds = true
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
